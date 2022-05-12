36678bf69338745fe7f75fafecb1c20ac3097553
Alexis Courties <alex_bits@protonmail.com>
2022-05-12 18:37:51 +0200

    initial bug fixes

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -5,7 +5,7 @@ import { mdiHome } from '@mdi/js'
 </script>
 
 <main>
-	<SvgIcon type="mdi" path={icon} {size}></SvgIcon>
+	<SvgIcon type="mdi" path={mdiHome}></SvgIcon>
 
 	<button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button>
 </main>
